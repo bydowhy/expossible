85f5b65e6f72ce2642b2e9b89606c69f459da85e
Mirza Alfarisi <67405261+bydowhy@users.noreply.github.com>
2025-02-09 13:56:58 +0700

    09022025

--- resources/js/Pages/Creator/Publication/Index.vue
+++ resources/js/Pages/Creator/Publication/Index.vue
@@ -18,37 +18,41 @@
     </div>
     <div class="bg-white rounded-md shadow overflow-x-auto">
       <table class="w-full whitespace-nowrap">
-        <tr class="text-left font-bold">
-          <th class="pb-4 pt-6 px-6">Title</th>
-          <th class="pb-4 pt-6 px-6">Topic</th>
-          <th class="pb-4 pt-6 px-6" colspan="2">Publication Date</th>
-        </tr>
-        <tr v-for="article in articles.data" :key="article.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
-          <td class="border-t">
-            <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/admin/publications/${article.id}/edit`">
-              {{ article.title }}
-              <icon v-if="article.deleted_at" name="trash" class="shrink-0 ml-2 w-3 h-3 fill-gray-400" />
-            </Link>
-          </td>
-          <td class="border-t">
-            <Link class="flex items-center px-6 py-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
-              {{ article.topic }}
-            </Link>
-          </td>
-          <td class="border-t">
-            <Link class="flex items-center px-6 py-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
-              {{ article.created_at }}
-            </Link>
-          </td>
-          <td class="w-px border-t">
-            <Link class="flex items-center px-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
-              <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
-            </Link>
-          </td>
-        </tr>
-        <tr v-if="articles.data.length === 0">
-          <td class="px-6 py-4 border-t" colspan="4">No publications found</td>
-        </tr>
+        <thead>
+          <tr class="text-left font-bold">
+            <th class="pb-4 pt-6 px-6">Title</th>
+            <th class="pb-4 pt-6 px-6">Topic</th>
+            <th class="pb-4 pt-6 px-6" colspan="2">Publication Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="article in articles.data" :key="article.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/admin/publications/${article.id}/edit`">
+                {{ article.title }}
+                <icon v-if="article.deleted_at" name="trash" class="shrink-0 ml-2 w-3 h-3 fill-gray-400" />
+              </Link>
+            </td>
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
+                {{ article.topic }}
+              </Link>
+            </td>
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
+                {{ article.created_at }}
+              </Link>
+            </td>
+            <td class="w-px border-t">
+              <Link class="flex items-center px-4" :href="`/admin/publications/${article.id}/edit`" tabindex="-1">
+                <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
+              </Link>
+            </td>
+          </tr>
+          <tr v-if="articles.data.length === 0">
+            <td class="px-6 py-4 border-t" colspan="4">No publications found</td>
+          </tr>
+        </tbody>
       </table>
     </div>
     <pagination class="mt-6" :links="articles.links" />
